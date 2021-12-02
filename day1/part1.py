84effe46daa03a68961706610e3dcf5f6fc17e2a
jonny4547 <joaoapereira21@hotmail.com>
2021-12-02 00:43:50 +0000

    Ignore output files

--- day1/part1.py
+++ day1/part1.py
@@ -1,6 +1,6 @@
 import itertools as it
 
-with open("input") as inf, open("part1", "w+") as outf:
+with open("input") as inf, open("part1.out", "w+") as outf:
     increases = sum(curr > prev for prev, curr in it.pairwise(map(int, inf)))
 
     outf.write(f"{increases}\n")
--- day1/part2.py
+++ day1/part2.py
@@ -1,6 +1,6 @@
 import itertools as it
 
-with open("input") as inf, open("part2", "w+") as outf:
+with open("input") as inf, open("part2.out", "w+") as outf:
     increases = sum(curr > prev for prev, curr in it.pairwise(map(sum, ((a, b, c) for (a, _), (b, c) in it.pairwise(it.pairwise(map(int, inf)))))))
 
     outf.write(f"{increases}\n")
